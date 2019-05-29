f0f2973399aad2e70ec2e074af7e0edc0cc3f15c
bencat07 <bencat07@gmail.com>
2019-05-29 21:00:51 +0200

    Start Repo

---
+++ src/log.h
@@ -0,0 +1,26 @@
+/*
+ * log.h
+ *
+ *  Created on: Aug 11, 2017
+ *      Author: nullifiedcat
+ */
+
+#pragma once
+
+#include <syslog.h>
+#include "header.h"
+
+#if LOG
+
+#define log(level, ...) \
+    openlog("libvpcfs", LOG_PID, LOG_USER); \
+    syslog(level, __VA_ARGS__); \
+    closelog(); \
+    printf(__VA_ARGS__); \
+    printf("\n");
+
+#else
+
+#define log(...)
+
+#endif
